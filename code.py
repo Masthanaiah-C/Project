54ffa3ab0091270fcbd49226b618852112bf5a0a
mast <ctmasthanaiah5757@gmail.com>
2020-08-28 21:56:30 +0530

    added cropped images and updated the progress of 28-08-2020

--- code.py
+++ code.py
@@ -1,6 +1,15 @@
 import cv2
-j=cv2.imread("/Users/DELL/Desktop/Project/ab.png")
-cv2.imshow("sucess",j)
-cv2.imwrite("/Users/DELL/Desktop/image.png",j)
-cv2.waitKey(0)
+new_y=int(input("y in in pixels:"))
+new_x=int(input(" x in pixels:"))
+b=cv2.imread("/Users/DELL/Desktop/Project/ab.png")
+m=b.shape
+
+y=m[0]//new_y
+x=m[1]//new_x
+for i in range(x):
+    for j in range(y):
+        k=b[new_y*j:new_y*(j+1), new_x*i:new_x*(i+1)]
+        cv2.imwrite("/Users/DELL/Desktop/Project/crop/"+str(j)+"-"+str(i)+".png",k)
+        
+
 cv2.destroyAllWindows()
